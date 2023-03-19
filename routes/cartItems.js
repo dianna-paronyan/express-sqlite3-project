abeaf857a36c413689870d8f96fda323eec6a535
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:29:28 +0400

    create cartItems route

--- routes/cartItems.js
+++ routes/cartItems.js
@@ -1,7 +1,13 @@
+const { authenticateUserToken } = require('../jwt/jwt_authenticate');
+
 function cartItems_route(app){
-    const cartItems_controller = require('../controllers/')
-    
-    app.get('/cartItems', cartItems_controller);
+    const cartItems_controller = require('../controllers/cartItems_controller');
+
+    app.get('/allCartItems', authenticateUserToken, cartItems_controller.allCartItems);
+    app.get('/cartItem', authenticateUserToken, cartItems_controller.cartItem);
+    app.post('/createCartItems', authenticateUserToken, cartItems_controller.createCartItems);
+    app.put('/updateCartItems/:id', authenticateUserToken, cartItems_controller.updateCartItems);
+    app.delete('/deleteCartItems/:id', authenticateUserToken, cartItems_controller.deleteCartItems);
 }
 
 module.exports = {

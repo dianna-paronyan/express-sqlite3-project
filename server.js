2b24e48854490a6a054e49f5facfd9d838577551
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:31:04 +0400

    call routes and controllers functions

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const { usersTable } = require('./models/users_schema');
-const { product_route } = require('./routes/product');
 const app = express();
-const {products_route} = require('./routes/products');
-const { users_route } = require('./routes/users');
 const port = require('./constants').port;
 const db = require('./index').db;
-const productsTable = require('./models/products_schema').productsTable;
+const { cartItemsTable } = require('./models/cartItems_schema');
+const { usersTable } = require('./models/users_schema');
+const { cart_route } = require('./routes/cart');
+const { cartItems_route } = require('./routes/cartItems');
+const { product_route } = require('./routes/product');
+const {products_route} = require('./routes/products');
+const { users_route } = require('./routes/users');
+const {cartTable} = require('./models/cart_schema')
+const {productsTable} = require('./models/products_schema');
 app.use(express.json());
 
 productsTable(db);
 usersTable(db);
+cartTable(db);
+cartItemsTable(db);
 
 app.get('/', (req,res)=>{
     res.send('hello')
@@ -19,5 +25,7 @@ app.get('/', (req,res)=>{
 products_route(app);
 product_route(app);
 users_route(app);
+cart_route(app);
+cartItems_route(app);
 
 app.listen(port);

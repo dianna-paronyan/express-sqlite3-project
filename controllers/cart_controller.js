8d2a9cc4fa716d189b032722cd2c3be6169edca6
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:23:43 +0400

    create cart controller

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -1,7 +1,28 @@
-const db = require('../index').db;
+const db = require("../index").db;
+const {checkUser}  = require('../jwt/checkIsUser')
 
-async function cart(req,res){
-
+async function cart(req, res) {
+  db.run("INSERT INTO cart(user_id) VALUES(?)", [req.body.user_id], (err) => {
+    res.send(JSON.stringify({ response: "created" }));
+  });
 }
 
-module.exports = {cart}
+async function userCart(req, res) {
+  const user = checkUser(req,res);
+  console.log(user)
+  db.all(
+    "SELECT * FROM users join cart on users.id = cart.user_id where cart.user_id = ?",
+    [req.body.user_id],
+    (err, data) => {
+      if (err) {
+        res.send(JSON.stringify({ response: "Something went wrong" }));
+      }
+      else if(user.id !== req.body.user_id){
+       return res.sendStatus(403);
+      }  
+        return res.send(data);
+    }
+  );
+}
+
+module.exports = { cart,userCart };

156a5f8501973773ed35204335d075dbda1e9233
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:29:46 +0400

    create products route

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
-const {authenticateToken} = require("../jwt/jwt_authenticate")
+const {authenticateAdminToken} = require("../jwt/jwt_authenticate")
 function products_route(app){
     const products_controller = require('../controllers/products_controller')
     
     app.get('/products', products_controller.allProducts);
-    app.post('/createProduct', authenticateToken, products_controller.createProduct);
-    app.put('/updateProduct/:id', authenticateToken, products_controller.updateProduct);
-    app.delete('/deleteProduct/:id', authenticateToken, products_controller.deleteProduct);
+    app.post('/createProduct', authenticateAdminToken, products_controller.createProduct);
+    app.put('/updateProduct/:id', authenticateAdminToken, products_controller.updateProduct);
+    app.delete('/deleteProduct/:id', authenticateAdminToken, products_controller.deleteProduct);
 }
 
 module.exports = {

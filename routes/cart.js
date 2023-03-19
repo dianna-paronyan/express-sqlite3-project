a53d5cf9c1ce69fb6fffb913265dc4a9a8dd5f18
dianna1234 <01234dianna@gmail.com>
2023-03-19 23:21:33 +0400

    add authenticateUser in cart

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ function cart_route(app) {
   const cart_controller = require("../controllers/cart_controller");
 
   app.get("/cartUser", authenticateUserToken, cart_controller.userCart);
-  app.post("/cart", cart_controller.cart);
+  app.post("/cart", authenticateUserToken, cart_controller.cart);
 }
 
 module.exports = {

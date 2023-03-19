0db31d318f5c99840b9f91ac8b7e40ccc175fb07
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:28:13 +0400

    create cartItems table

--- models/cartItems_schema.js
+++ models/cartItems_schema.js
@@ -1,6 +1,6 @@
-function cartItems(db){
+function cartItemsTable(db){
     db.run(
-        "CREATE TABLE IF NOT EXISTS cartItems(id INTEGER PRIMARY KEY, cart_id INTEGER NOT NULL,FOREIGN KEY (cart_id) REFERENCES cart(id), product_id INTEGER NOT NULL,FOREIGN KEY (product_id) REFERENCES products(id))"
+        "CREATE TABLE IF NOT EXISTS cartItems(id INTEGER PRIMARY KEY, cart_id INTEGER NOT NULL, product_id INTEGER NOT NULL, FOREIGN KEY (cart_id) REFERENCES cart(id), FOREIGN KEY (product_id) REFERENCES products(id))"
       );
 }
-module.exports = {cartItems}
+module.exports = {cartItemsTable}

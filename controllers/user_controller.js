9da7097c3b0a7b4e6c6791ab326fcdf868c3814f
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:25:41 +0400

    replace crypto with bcrypt

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,7 @@
 const db = require('../index').db;
 const {generateAccessToken} = require('../jwt/jwt_generate')
 const CryptoJS = require('crypto-js');
+const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -8,7 +9,9 @@ require('dotenv').config();
 async function register(req,res){
     const username = req.body.username;
     const password = req.body.password;
-    const hashed_password = CryptoJS.SHA256(password).toString();
+    // const hashed_password = CryptoJS.SHA256(password).toString()
+    const salt = await bcrypt.genSalt(10);
+    const hashed_password = await bcrypt.hash(password, salt)
     const sql = 'INSERT INTO users(username, password,role) VALUES (?,?,?)';
     db.run(sql, [username,hashed_password, 0],(err)=>{
         if(err){
@@ -21,12 +24,14 @@ async function register(req,res){
 async function login(req,res){
     const username = req.body.username;
     const password = req.body.password;
-    const hashed_password = CryptoJS.SHA256(password).toString();
+    // const hashed_password = CryptoJS.SHA256(password).toString();
+
     const sql = 'SELECT * FROM users WHERE username=?';
-    db.get(sql,[username],(err,row)=>{
-        if(username === row.username && hashed_password === row.password){
-            console.log(row.password);
-            const token = generateAccessToken(username,row.role);
+    db.get(sql,[username],await function(err,row){
+        // if(username === row.username && hashed_password === row.password){
+            if(username === row.username && bcrypt.compare(password, row.password)){
+        console.log(row.password);
+            const token = generateAccessToken(username,row.role,row.id);
             res.send(JSON.stringify({status: 'Logged in', jwt: token}));
        }else{
            res.send(JSON.stringify({status:'Wrong credentials'}));

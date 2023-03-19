0b9c32bf3b855d38fee943da20a9a3118295a041
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:28:52 +0400

    create cart route

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,9 @@
+const { authenticateUserToken } = require('../jwt/jwt_authenticate');
 function cart_route(app){
-    const cart_controller = require('../controllers/')
+    const cart_controller = require('../controllers/cart_controller')
     
-    app.get('/cart', cart_controller);
+    app.get('/cartUser',authenticateUserToken,  cart_controller.userCart)
+    app.post('/cart', cart_controller.cart);
 }
 
 module.exports = {

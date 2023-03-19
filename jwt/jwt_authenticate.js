3bd11fb06518fe6171c634468d8139e77239c0e3
dianna1234 <01234dianna@gmail.com>
2023-03-19 16:27:14 +0400

    create user authentication

--- jwt/jwt_authenticate.js
+++ jwt/jwt_authenticate.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const SECRET = process.env.SECRET;
 
-function authenticateToken(req, res, next) {
+function authenticateAdminToken(req, res, next) {
     const token = req.headers.authorization;
     console.log(token);
     if (token == null){
@@ -21,7 +21,26 @@ function authenticateToken(req, res, next) {
     })
   }
 
+  function authenticateUserToken(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token == null){
+        return res.sendStatus(401)
+    } 
+    
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403)
+      }
+        console.log(user);
+        if(user.username !== 'admin' && user.role === 0 ){
+          next()
+        }
+    })
+  }
+
   
 module.exports = {
-    authenticateToken
+  authenticateAdminToken,
+  authenticateUserToken
 }
